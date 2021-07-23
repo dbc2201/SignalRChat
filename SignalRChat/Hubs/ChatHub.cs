4e9dbee6ce4b73dee685522d1d439bb9cd8c0385
dbc2201 <divyanshb22@gmail.com>
2021-07-24 00:19:06 +0530

    add comments

--- SignalRChat/Hubs/ChatHub.cs
+++ SignalRChat/Hubs/ChatHub.cs
@@ -3,8 +3,10 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace SignalRChat.Hubs
 {
+    // The Hub class manages connections, groups, and messaging.
     public class ChatHub : Hub
     {
+        // This SendMessage() method can be called by a connected client to send a message to all clients.
         public async Task SendMessage(string userName, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", userName, message);
